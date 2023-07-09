4dcb626603da4e83960a652e8ea0a48aef69a61c
huangfuhuijie <787936777@qq.com>
2023-07-09 23:36:42 +0800

    add different visualization

--- example/RED/REDCNN.py
+++ example/RED/REDCNN.py
@@ -139,6 +139,42 @@ class RED_CNN_all_relu(nn.Module):
         out = self.relu(out)
         return out
 
+    def forward_return_mid(self, x):
+        # encoder
+        residual_1 = x
+        out = self.relu(self.conv1(x))
+        out = self.relu(self.conv2(out))
+        # out = self.conv1(x)
+        # out = self.conv2(out)
+
+        residual_2 = out
+        out = self.relu(self.conv3(out))
+        out = self.relu(self.conv4(out))
+        # out = self.conv3(out)
+        # out = self.conv4(out)
+
+        residual_3 = out
+        out = self.relu(self.conv5(out))
+        # out = self.conv5(out)
+        # decoder
+        ####这里改了，提取多个输出
+        out = self.tconv1(out)
+        out += residual_3
+        out = self.tconv2(self.relu(out))
+        out = self.tconv3(self.relu(out))
+        # out = self.tconv2(out)
+        # out = self.tconv3(out)
+        out += residual_2
+        input1 = out
+        out = self.tconv4(self.relu(out))
+        out = self.tconv5(self.relu(out))
+        # out = self.tconv4(out)
+        # out = self.tconv5(out)
+        out += residual_1
+        input2 = residual_1
+        out = self.relu(out)
+        return input1,input2,out
+
 
 class RED_CNN_last_relu(nn.Module):
     def __init__(self, out_ch=96):
--- example/visualization.py
+++ example/visualization.py
@@ -26,8 +26,12 @@ def visual():
     context.initializeContext("QAT")
     # net = RED_CNN()
     # net.load_state_dict(torch.load('./example/checkpoint/epoch_99_loss_0.008091.pth'))
+    # net = RED_CNN_all_relu()
+    # net.load_state_dict(torch.load('./example/checkpoint/best_param_all_relu.pth'))
     net = RED_CNN_last_relu()
-    net.load_state_dict(torch.load('./example/checkpoint/best_param_last_relu_2.pth'))
+    net.load_state_dict(torch.load('./example/checkpoint/best_param_last_relu.pth'))
+
+
     # net = RED_CNN_all_relu()
     # net.load_state_dict(torch.load('./example/checkpoint/best_param_all_relu.pth'))
 
@@ -37,10 +41,15 @@ def visual():
     img_data = np.expand_dims(img_data, axis=0)
     # img_data = np.random.random((1,4,4))
     ori_shape = img_data.shape
-    label = cv.imread("example/RED/data_1927.jpg",0) # 255*255,[0-255]
-    label = label/255 # 255*255,[0-1]
-    label = np.expand_dims(label,0) # 1*255*255,[0-255]
-    label = np.expand_dims(label,0) # 1*1*255*255,[0-255]
+    # label = cv.imread("example/RED/data_1927.jpg",0) # 255*255,[0-255]
+    # label = label/255 # 255*255,[0-1]
+    # label = np.expand_dims(label,0) # 1*255*255,[0-255]
+    # label = np.expand_dims(label,0) # 1*1*255*255,[0-255]
+    label_path = './example/RED/data_1927_label.mat'
+    label = scio.loadmat(label_path)['data']
+    label = np.expand_dims(label, axis=0)
+    label = np.expand_dims(label, axis=0)
+    label = label/label.max()
 
     # validate in torch
     input_data = torch.FloatTensor(img_data).unsqueeze_(0)
@@ -59,8 +68,9 @@ def visual():
     tr_ct_out_data = np.array([[[float(data2.ciphertext().getTexts()[0].__str__())for data2 in data1]for data1 in data] for data in ct_out_data])
     tr_ct_out_data = tr_ct_out_data/np.max(tr_ct_out_data)
 
-    save_plts(2,3,"last_relu.png",displaywin(img_data.transpose(1,2,0)),displaywin(out.transpose(1,2,0)),label.squeeze(0).transpose(1,2,0),tr_ct_img_data.transpose(1,2,0),tr_ct_out_data.transpose(1,2,0),gray=True)
-
+    scio.savemat("./result_last_relu_all.mat", {"input":img_data,"result":out,"label":label,"tr_ct_img":tr_ct_img_data,"tr_ct_out_data":tr_ct_out_data,"psnr":psnr,"ssim":ssim})
+    # save_plts(2,3,"last_relu_"+str(psnr[0])+"_"+str(ssim[0])+".png",displaywin(img_data.transpose(1,2,0)),displaywin(out.transpose(1,2,0)),displaywin(label.squeeze(0).transpose(1,2,0)),tr_ct_img_data.transpose(1,2,0),tr_ct_out_data.transpose(1,2,0),gray=True)
+    save_plts(2,3,"last_relu_"+str(psnr[0])+"_"+str(ssim[0])+".png",img_data.transpose(1,2,0),out.transpose(1,2,0),label.squeeze(0).transpose(1,2,0),tr_ct_img_data.transpose(1,2,0),tr_ct_out_data.transpose(1,2,0),gray=True)
     
 
     context.terminateContext()
